695b7ff7bb9b38e77e3a99e9e797c17ff708c060
Andrea Aspesi <aspex2014@gmail.com>
2022-02-23 21:16:49 +0100

    First implementation of tero instance block and counter block

--- Sources/RTL/TERO_instance.sv
+++ Sources/RTL/TERO_instance.sv
@@ -19,14 +19,136 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module TERO_instance(
-        input clk,
+        input clk,      // System clock
 
-        input enable_1,
-        input enable_2,
+        input enable_1, // If asseted, starts the oscillation of the first TERO loop
+        input enable_2, // If asseted, starts the oscillation of the second TERO loop
 
-        output dout_1,
-        output dout_2
+        output dout_1,  // Oscillating output of the first TERO loop
+        output dout_2   // Oscillating output of the second TERO loop
     );
+
+    wire loop1_enable1; // 1 of 2 enable signals of for the first loop
+    wire loop1_enable2; // 2 of 2 enable signals of for the first loop
+    wire loop1_inv1;    // Output of the interter 1 for the first loop
+    wire loop1_inv2;    // Output of the interter 2 for the first loop
+    wire loop1_and1;    // Output of the and 1 for the first loop
+    wire loop1_and2;    // Output of the and 2 for the first loop
+
+
+    wire loop2_enable1; // 1 of 2 enable signals of for the second loop
+    wire loop2_enable2; // 2 of 2 enable signals of for the second loop
+    wire loop2_inv1;    // Output of the interter 1 for the second loop
+    wire loop2_inv2;    // Output of the interter 2 for the second loop
+    wire loop2_and1;    // Output of the and 1 for the second loop
+    wire loop2_and2;    // Output of the and 2 for the second loop
+
+
+    // ---------------------------
+    //      First TERO Loop
+    // ---------------------------
+
+    // Enable buffers of loop1
+    FDRE #(
+        .INIT(1'b0)         // start at 0
+    ) loop1_enable1_buff (
+        .Q(loop1_enable1), // output
+        .C(clk),
+        .CE(1'b1),          // always enabled
+        .R(1'b0),           // never reset
+        .D(enable_1)        // input
+    );
+    FDRE #(
+        .INIT(1'b0)         // start at 0
+    ) loop1_enable2_buff (
+        .Q(loop1_enable2), // output
+        .C(clk),
+        .CE(1'b1),          // always enabled
+        .R(1'b0),           // never reset
+        .D(enable_1)        // input
+    );
+
+    // Blocks of loop1 (AND + NOT)
+    LUT6_2 #(
+        .INIT(64'hcccc000000ff00ff)   // block configuration
+    ) loop1_block1 (    
+        .O6(loop1_and1),              // the upper LUT5 is I4 AND I1, assigned to O6                
+        .O5(loop1_inv1),              // the lower LUT5 is NOT I3, assigned to O5            
+        .I0(1'b0),
+        .I1(loop1_enable1),
+        .I2(1'b0),
+        .I3(loop1_and1),              // self loop on the and (as now must enter in the inverter in the lower LUT5)
+        .I4(loop1_inv2),              // inverter of the second block of this loop
+        .I5(1'b1)                     // select upper LUT5 as O6 (in order to use both LUT5)
+    );
+    LUT6_2 #(
+        .INIT(64'hcccc000000ff00ff)   // block configuration
+    ) loop1_block2 (    
+        .O6(loop1_and2),              // the upper LUT5 is I4 AND I1, assigned to O6                
+        .O5(loop1_inv2),              // the lower LUT5 is NOT I3, assigned to O5            
+        .I0(1'b0),
+        .I1(loop1_enable2),
+        .I2(1'b0),
+        .I3(loop1_and2),              // self loop on the and (as now must enter in the inverter in the lower LUT5)
+        .I4(loop1_inv1),              // inverter of the first block of this loop
+        .I5(1'b1)                     // select upper LUT5 as O6 (in order to use both LUT5)
+    );
+
+    // Link output
+    assign dout_1 = loop1_and1;
+
+    // ---------------------------
+    //      Second TERO Loop
+    // ---------------------------
+
+    // Enable buffers of loop2
+    FDRE #(
+        .INIT(1'b0)         // start at 0
+    ) loop2_enable1_buff (
+        .Q(loop2_enable1), // output
+        .C(clk),
+        .CE(1'b1),          // always enabled
+        .R(1'b0),           // never reset
+        .D(enable_2)        // input
+    );
+    FDRE #(
+        .INIT(1'b0)         // start at 0
+    ) loop2_enable2_buff (
+        .Q(loop2_enable2), // output
+        .C(clk),
+        .CE(1'b1),          // always enabled
+        .R(1'b0),           // never reset
+        .D(enable_2)        // input
+    );
+
+    // Blocks of loop2 (AND + NOT)
+    LUT6_2 #(
+        .INIT(64'hf000f0000000ffff)   // block configuration
+    ) loop2_block1 (    
+        .O6(loop2_and1),              // the upper LUT5 is I4 AND I1, assigned to O6                
+        .O5(loop2_inv1),              // the lower LUT5 is NOT I3, assigned to O5            
+        .I0(1'b0),
+        .I1(1'b0),
+        .I2(loop2_enable1),
+        .I3(loop2_inv2),              // inverter of the second block of this loop
+        .I4(loop2_and1),              // self loop on the and (as now must enter in the inverter in the lower LUT5)
+        .I5(1'b1)                     // select upper LUT5 as O6 (in order to use both LUT5)
+    );
+    LUT6_2 #(
+        .INIT(64'hf000f0000000ffff)   // block configuration
+    ) loop2_block2 (    
+        .O6(loop2_and2),              // the upper LUT5 is I4 AND I1, assigned to O6                
+        .O5(loop2_inv2),              // the lower LUT5 is NOT I3, assigned to O5            
+        .I0(1'b0),
+        .I1(1'b0),
+        .I2(loop2_enable2),
+        .I3(loop2_inv1),              // inverter of the first block of this loop 
+        .I4(loop2_and2),              // self loop on the and (as now must enter in the inverter in the lower LUT5)
+        .I5(1'b1)                     // select upper LUT5 as O6 (in order to use both LUT5)
+    );
+
+    // Link output
+    assign dout_2 = loop2_and1;
+
 endmodule
--- Sources/RTL/counter.sv
+++ Sources/RTL/counter.sv
@@ -19,17 +19,44 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
+// Ripple counter with async reset
 module counter#(
     parameter NUM_BITS = 32
-)
+) (
+    input arst,                     // Asynchronous reset of the counter
 
-(
-    input arst,
+    input signal_in,                // Oscillating signal to be counted (counts the rising endge)
+    input enable,                   // When asserted, counting is enabled
 
-    input signal_in,
-    input enable,
-
-    output [NUM_BITS-1 : 0] count
+    output [NUM_BITS-1 : 0] count   // Output of the counter (binary encoding)
 );
+
+    wire [NUM_BITS-1 : 0] q;
+    wire [NUM_BITS-1 : 0] d;
+    wire [NUM_BITS : 0] t;
+
+    assign t[0] = signal_in;    // the first block is linked to the signal
+
+    genvar i;
+    generate // Generate ripple counter with FFD
+       for (i=NUM_BITS-1; i >= 0; i=i-1)
+       begin: gen_ripple_block
+            assign d[i] = ~q[i];  // self-loop for this block
+            assign t[i+1] = d[i]; // link the block with the next
+            // Generate the block
+            FDCE #(
+                .INIT(1'b0)
+            ) FDCE_inst (
+                .Q(q[i]),
+                .C(t[i]),
+                .CE(enable),
+                .CLR(arst),
+                .D(d[i])
+            );
+       end : gen_ripple_block
+    endgenerate
+
+    // Link output
+    assign count = q;
+
 endmodule
