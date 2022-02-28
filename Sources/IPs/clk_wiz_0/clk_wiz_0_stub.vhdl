3427a3dc7515a1fefc9d4f979400c4ac25a61cb8
Alessandro Putortì <putoale@gmail.com>
2022-03-01 00:18:47 +0100

    added clk wizard

---
+++ Sources/IPs/clk_wiz_0/clk_wiz_0_stub.vhdl
@@ -0,0 +1,31 @@
+-- Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2020.2 (lin64) Build 3064766 Wed Nov 18 09:12:47 MST 2020
+-- Date        : Mon Feb 28 23:50:43 2022
+-- Host        : putoale-MS-7B86 running 64-bit Ubuntu 21.10
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/putoale/Documenti/PUF_proj/puf-fpga/Sources/IPs/clk_wiz_0/clk_wiz_0_stub.vhdl
+-- Design      : clk_wiz_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tcpg236-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity clk_wiz_0 is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    reset : in STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end clk_wiz_0;
+
+architecture stub of clk_wiz_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,reset,locked,clk_in1";
+begin
+end;
