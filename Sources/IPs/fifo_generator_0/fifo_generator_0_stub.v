b964f2c2d0c4ff45b9bc0ba6a079012f7929ec20
Alessandro Putortì <putoale@gmail.com>
2022-03-02 14:20:02 +0100

    fixed fifo_generator settings

--- Sources/IPs/fifo_generator_0/fifo_generator_0_stub.v
+++ Sources/IPs/fifo_generator_0/fifo_generator_0_stub.v
@@ -1,10 +1,10 @@
 // Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2020.2 (lin64) Build 3064766 Wed Nov 18 09:12:47 MST 2020
-// Date        : Sun Feb 27 17:50:19 2022
+// Date        : Wed Mar  2 10:40:23 2022
 // Host        : putoale-MS-7B86 running 64-bit Ubuntu 21.10
-// Command     : write_verilog -force -mode synth_stub -rename_top fifo_generator_0 -prefix
-//               fifo_generator_0_ fifo_generator_0_stub.v
+// Command     : write_verilog -force -mode synth_stub
+//               /home/putoale/Documenti/PUF_proj/puf-fpga/Sources/IPs/fifo_generator_0/fifo_generator_0_stub.v
 // Design      : fifo_generator_0
 // Purpose     : Stub declaration of top-level module interface
 // Device      : xc7a35tcpg236-1
